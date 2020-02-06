4efd66aca2eb7f0730a1cfb02ce716ca8c272ae6
Srivamsi.s <sakirepalli.srivamsi@gmail.com>
2020-02-06 16:42:27 +0530

    class vehicle with changes

--- vehicle.py
+++ vehicle.py
@@ -42,16 +42,25 @@ class parking_lot:
                 if self.lot_list[i] == 0:
                     return i
 
+    def get_slot_details(self, spotID):
+        data = {}
+        vehicle = self.lot_list[spotID]
+        data['vehicle_regNo'] = vehicle.regNo
+        data['colour'] = vehicle.colour
+        data['vehicle_type'] = vehicle.vehicle_type
+        return data
 
 # ticket_dict = dict()
 
 class ticket_dict_class:
-    def __init__(self):
-        self.dictionary = dict()
-        # pass
+    # dictionary = dict()
 
-    def create_dict(self):
-        return self.dictionary
+    def __init__(self):
+        # self.dictionary = dict()
+        pass
+
+    def get_dictionary(self):
+        return dict()
 
 
 def add_ticket(ticket, ticket_dictionary):
@@ -60,7 +69,7 @@ def add_ticket(ticket, ticket_dictionary):
 
 class Ticket:
 
-    def __init__(self, vehicle, spotID):
+    def __init__(self, spotID):
         self.ticketID = uuid.uuid4()
         self.checkinTime = datetime.now()
         self.checkoutTime = str()
@@ -84,8 +93,8 @@ class Ticket:
     def get_ticketID(self):
         return self.ticketID
 
-    def checkout(self, ticketID, ticket_dict, lot_list):
-        ticket = ticket_dict[ticketID]
+    def checkout(self, ticket_dict, lot_list):
+        ticket = ticket_dict[str(self.ticketID)]
         ticket.checkoutTime = datetime.now()
         ticket_dict[ticketID] = ticket
         spotID = ticket.spotID
@@ -93,17 +102,20 @@ class Ticket:
 
     def get_all_values(self):
         data = {
-                # 'ticketID': self.ticketID,
-                'checkinTime': self.checkinTime,
-                # 'vehicle_reg_no': self.vehicle_reg_no,
-                # 'vehicle_type': self.vehicle_type,
-                # 'vehicle_colour': self.vehicle_colour,
-                'spotID': self.spotID
-                }
+            # 'ticketID': self.ticketID,
+            'checkinTime': self.checkinTime,
+            # 'vehicle_reg_no': self.vehicle_reg_no,
+            # 'vehicle_type': self.vehicle_type,
+            # 'vehicle_colour': self.vehicle_colour,
+            'spotID': self.spotID
+        }
         return data
 
 
-ticket_dict = ticket_dict_class.create_dict()
+ticket_dict_instance = ticket_dict_class
+ticket_dict = ticket_dict_instance.get_dictionary()
+
+# ticket_dict = dict()
 
 lot = parking_lot(10)
 l1 = lot.get_parking_list
@@ -119,7 +131,7 @@ lot.add_spot(empty_spotID, v1)
 print(lot.lot_list)
 
 # ticket1 = Ticket().create_ticket(v1, empty_spotID, ticket_dict)
-ticket1 = Ticket(v1, empty_spotID)
+ticket1 = Ticket(empty_spotID)
 # ticket_dict = ticket_dict.create_dict()
 # ticket_dict_class.add_ticket(ticket1, ticket_dict)
 add_ticket(ticket1, ticket_dict)
@@ -137,7 +149,7 @@ empty_spotID = lot.find_empty_spot()
 lot.add_spot(empty_spotID, v2)
 print(lot.lot_list)
 
-ticket2 = Ticket(v2, empty_spotID)
+ticket2 = Ticket(empty_spotID)
 add_ticket(ticket2, ticket_dict)
 
 print(ticket2.get_all_values())
@@ -151,7 +163,7 @@ empty_spotID = lot.find_empty_spot()
 lot.add_spot(empty_spotID, v3)
 print(lot.lot_list)
 
-ticket3 = Ticket(v3, empty_spotID)
+ticket3 = Ticket(empty_spotID)
 add_ticket(ticket3, ticket_dict)
 
 print(ticket3.get_all_values())
@@ -159,6 +171,21 @@ ticketID = str(ticket3.get_ticketID())
 print(ticket_dict[ticketID])
 
 print('---vehicle2-checkout---')
-ticket3.checkout(ticket2.spotID, ticket_dict, l1)
+ticket2.checkout(ticket_dict, lot.lot_list)
+# print('abcde')
+print(lot.lot_list)
+print(lot.get_slot_details(0))
 
-print(l1)
+def add_vehicle(ticket_dict, veh_colour, veh_regNo, veg_type):
+    vehicle_object = Vehicle(veh_regNo, veh_colour, veg_type)
+    empty_spotID = lot.find_empty_spot()
+    lot.add_spot(empty_spotID, vehicle_object)
+    ticket_object = Ticket(empty_spotID)
+    add_ticket(ticket_object, ticket_dict)
+    print("Ticket successfully created")
+    print(ticket_object.get_all_values())
+    print()
+    return ticket_object.ticketID
+
+
+print(add_vehicle(ticket_dict, 'blue', 'qwer8465', '0'))
