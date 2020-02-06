2d099e01aeec061df59f87750f09830f15525aa1
kavya saxena <kavya.saxena@lendingkart.com>
2020-02-06 20:55:24 +0530

    Updated Tickets.py

--- src/Parking_lot.py
+++ src/Parking_lot.py
@@ -1,9 +1,18 @@
 import src.Vehicle as vehicle
 import datetime
 
+
 class Parking_lot:
 
-    def __init__(self, size):
+    def __init__(self):
+        self.size = 0
+        self.lot_list = [0] * 0
+        self.occupied_count = -1
+        self.slotID = -1
+        self.inTime = 0
+        self.outTime = 0
+
+    def create_parking_lot(self, size):
         self.size = size
         self.lot_list = [0] * size
         self.occupied_count = -1
@@ -31,17 +40,15 @@ class Parking_lot:
         else:
             return -1
 
-    def carCheckOut(self):
+    def carCheckOut(self, slot):
         self.occupied_count -= 1
-        self.lot_list[self.slotID] = -1
+        self.lot_list[slot] = -1
         self.outTime = datetime.datetime.now()
 
     def find_my_car(self, regno):
         if self.occupied_count == 0:
             return -1
         else:
-            for x in range(0,self.size):
+            for x in range(0, self.size):
                 if self.lot_list[x] == regno:
-                    return  x
-
-
+                    return x
---
+++ src/Tickets.py
@@ -0,0 +1,33 @@
+import uuid
+import src.Parking_lot as park
+
+
+class Tickets:
+    def __init__(self):
+        self.park = park()
+        self.ticketID = -1
+        self.flag = 0
+        self.lot_dict = {}
+        self.success = False
+
+    def raise_ticket(self, size, regno):
+        if self.flag == 0:
+            park.Parking_lot.create_parking_lot(size)
+            flag = 1
+        slot = park.Parking_lot.carParked(regno)
+        if slot != -1:
+            self.ticketID = uuid.uuid4()
+            self.lot_dict[self.ticketID] = slot
+            self.success = True
+        if self.success:
+            return self.ticketID
+        else:
+            return -1
+
+    def close_ticket(self, ticketID):
+        slot = self.lot_dict.get(ticketID)
+        park.Parking_lot.carCheckOut(slot)
+        del self.lot_dict[ticketID]
+
+    def remove_parking_lot(self):
+        self.flag = 0
